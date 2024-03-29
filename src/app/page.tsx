cec771b9bb78a095ad38b941cd8ad28b9beb0797
Derek J. Kinsman <derek@unttld.com>
2024-03-29 10:16:22 -0400

    Updates.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,48 @@
 import React from "react";
 import { Metadata } from 'next';
+import page from '@styles/components/Page.module.scss';
 
 export const metadata: Metadata = {
   title: 'Xandering the Dungeon',
-  description: 'Allegedly.'
+  description: "It's 'Jaquaysing' and only 'Jaquaysing'.",
+
+  openGraph: {
+    title: 'Xandering the Dungeon',
+    description: "It's 'Jaquaysing' and only 'Jaquaysing'.",
+    url: 'https://xanderingthedungeon.com/',
+    siteName: 'Xandering the Dungeon',
+    images: [
+      {
+        url: 'https://xanderingthedungeon.com/card.png', // Must be an absolute URL
+        width: 1280,
+        height: 628,
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Xandering the Dungeon',
+    description: "It's 'Jaquaysing' and only 'Jaquaysing'.",
+    creator: '@derekkinsman',
+    images: ['https://xanderingthedungeon.com/card.png'], // Must be an absolute URL
+  },
 }
 
 export default function Page() {
-  return <>
-    <h1>Xandering [zan-der-ing]</h1>
-    <p>verb</p>
-    <ol>
-      <li>to take credit for something someone else has made.</li>
-    </ol>
-    <p>Words that May Be Confused with Xandering</p>
-    <p><a href='https://jaquaysingthedungeon.com/'>Jaquaysing</a></p>
-  </>
+  return <section className={`${page.section}`}>
+    <section className={`${page.word}`}>
+      <h1 className={`${page.title}`}>Xandering [zan-der-ing]</h1>
+      <p className={`${page.type}`}>verb</p>
+      <ol className={`${page.definitions}`}>
+        <li className={`${page.definition}`}>to take credit for something someone else has made.</li>
+      </ol>
+    </section>
+    <section className={`${page.additional}`}>
+      <p className={`${page.confusion}`}>Words that May Be Confused with Xandering:</p>
+      <p className={`${page.wordlist}`}><a href='https://jaquaysingthedungeon.com/'>Jaquaysing</a></p>
+    </section>
+  </section>
 }
